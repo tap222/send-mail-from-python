8e182ead470a9b06c3c5efee7d898d1e1a40cdd5
tap222 <mtapas.mohanty95@gmail.com>
2017-04-24 13:38:57 +0530

    Create E-mail .py

---
+++ E-mail .py
@@ -0,0 +1,10 @@
+import smtplib
+import logging
+ 
+server = smtplib.SMTP('smtp.gmail.com', 587)
+server.starttls()
+server.login("YOUR EMAIL ADDRESS", "YOUR PASSWORD")
+ 
+msg = "YOUR MESSAGE!"
+server.sendmail("YOUR EMAIL ADDRESS", "THE EMAIL ADDRESS TO SEND TO", msg)
+server.quit()
